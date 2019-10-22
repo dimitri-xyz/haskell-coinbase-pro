decb6815c02910add73f127e719068f0f4817115
Eric Pashman <eric.pashman@gmail.com>
2019-10-22 14:39:09 -0500

    Use Aeson's default-value combinator, clarify commment on when parsing results in Nothing default

--- src/Coinbase/Exchange/Types/Socket.hs
+++ src/Coinbase/Exchange/Types/Socket.hs
@@ -192,7 +192,7 @@ instance FromJSON ExchangeMessage where
                     Just _ -> limit <|> market
             "received" -> do
                 typ  <- m .:  "order_type"
-                mcid <- (m .:? "client_oid") <|> pure Nothing -- parsing never fails here, opaque field if cannot parse UUID
+                mcid <- (m .:? "client_oid") .:! Nothing -- Set to Nothing when field is present but UUID parse fails, typically when field is set to empty string
                 case typ of
                     Limit -> ReceivedLimit
                                 <$> m .: "time"
